699eec2ed8822793d48b3a0425e83a4679f6c66f
Abhishek Dhakal <abhishekdhakal1826@gmail.com>
2024-11-12 23:32:46 +0545

    +card

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation";
 import SideBar from "./components/SideBar";
 import Slider from "./components/Slider";
 import Card from "./components/Card";
+import imagee from "./assets/img.png";
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Navigation />
       <SideBar />
       <Slider />
-      <Card discount={"20"}/>
+      <Card discount={"20"} image={imagee}/>
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,35 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { FaHeart, FaEye } from "react-icons/fa";
+import axios from "axios";
 
 function Card({ discount, image, name, price, rate, id }) {
   const [cart, setCart] = useState([]);
+  const [products, setProducts] = useState([]);
+
   function addToCart(id) {
-    setCart([id, ...cart]);
+    setCart((prevCart) => [id, ...prevCart]); // Update cart properly
   }
+
+  useEffect(() => {
+    axios
+      .get("https://fake-store-api.mock.beeceptor.com/api/products")
+      .then((res) => {
+        setProducts(res.data); // Access the data property of the response
+      })
+      .catch((err) => console.error("Error fetching data:", err)); // Add error handling
+  }, []);
+
   return (
-    <>
-      <div className="h-[300px] w-64 border-solid border-2 border-red-700 p-4">
-        <div className="flex justify-between">
-          <div className="rounded-lg bg-red-500 flex items-center text-white px-5">
-            -{discount}%
-          </div>
-          <div></div>
-          <div>
-            <ul>
-              <li>Heart</li>
-              <li>Eye</li>
-            </ul>
-          </div>
+    <div
+      className="h-[300px] w-64 border border-solid border-red-700 p-4 rounded-lg flex flex-col justify-between bg-cover bg-center text-white"
+      style={{ backgroundImage: `url(${image})` }}
+    >
+      {/* Top Section with Discount Badge and Icons */}
+      <div className="flex justify-between items-center">
+        {/* Discount Badge */}
+        <div className="rounded-lg bg-red-500 flex items-center text-white px-3 py-1">
+          -{discount}%
         </div>
-        <button
-          className="bg-black text-white"
-          onClick={() => {
-            addToCart(id);
-          }}
-        >
-          Add to cart
-        </button>
+
+        {/* Icons */}
+        <div className="flex flex-col space-y-2">
+          <button className="hover:text-red-500">
+            <FaHeart />
+          </button>
+          <button className="hover:text-blue-500">
+            <FaEye />
+          </button>
+        </div>
       </div>
-    </>
+
+      {/* Add to Cart Button */}
+      <button
+        className="bg-black bg-opacity-80 rounded py-2 mt-4 w-full hover:bg-opacity-90"
+        onClick={() => addToCart(id)}
+      >
+        Add to cart
+      </button>
+
+      {/* Display fetched product names */}
+      <div>
+        {products.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </div>
+    </div>
   );
 }
 
