eb7bc6300ddfbd01ffd4e617b5772c27a03b20e1
Abhishek Dhakal <abhishekdhakal1826@gmail.com>
2024-11-23 17:29:44 +0545

    icons for category

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation";
 import SideBar from "./components/SideBar";
 import Slider from "./components/Slider";
 import Card from "./components/Card";
+import Categories from "./components/Categories";
 //import Signup from "./components/signup/Signup";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <SideBar />
       <Slider />
       <Card />
+      <Categories />
       {/* <Signup /> */}
     </>
   );
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,14 @@
-function Categories(){
- return <>
- <h1>Browse by Category</h1>
- <div className="flex justify-between">
-    <li>Phone</li>
-    <li>Camera</li>
-    <li>Tv</li>
- </div>
- </>
+import { IoPhonePortraitOutline } from "react-icons/io5";
+import { MdPhonelink } from "react-icons/md";
+function Categories() {
+  return (
+    <>
+      <h1 className="text-3xl">Browse by Category</h1>
+      <div className="flex justify-between text-2xl">
+        <IoPhonePortraitOutline />
+        <MdPhonelink />
+      </div>
+    </>
+  );
 }
-export default Categories
+export default Categories;
