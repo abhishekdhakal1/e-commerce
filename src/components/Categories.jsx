f22644b4a0076ff1bd3a6b0507a776e209588ea3
Abhishek Dhakal <abhishekdhakal1826@gmail.com>
2024-11-24 14:27:26 +0545

    j-a

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,7 +4,7 @@ function Categories() {
   return (
     <>
       <h1 className="text-3xl">Browse by Category</h1>
-      <div className="flex justify-between text-2xl">
+      <div className="flex justify-around text-2xl">
         <IoPhonePortraitOutline />
         <MdPhonelink />
       </div>
