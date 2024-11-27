f1e78b210735d548685e2f4db8c9d194faa6b18d
Abhishek Dhakal <abhishekdhakal1826@gmail.com>
2024-11-27 19:26:37 +0545

    btn +

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,8 +2,10 @@ import { IoPhonePortraitOutline } from "react-icons/io5";
 import { MdPhonelink, MdOutlineCameraAlt } from "react-icons/md";
 import { GiHeadphones, GiGamepad } from "react-icons/gi";
 import { BsSmartwatch } from "react-icons/bs";
+import { useState } from "react";
 
 function Categories() {
+  const [viewAll, setViewAll] = useState(false);
   const categories = [
     { name: "Phones", icon: <IoPhonePortraitOutline /> },
     { name: "Computers", icon: <MdPhonelink /> },
@@ -12,24 +14,52 @@ function Categories() {
     { name: "HeadPhones", icon: <GiHeadphones /> },
     { name: "Gaming", icon: <GiGamepad /> },
   ];
+  const category = [
+    { name: "Phones", icon: <IoPhonePortraitOutline /> },
+    { name: "Computers", icon: <MdPhonelink /> },
+    { name: "SmartWatch", icon: <BsSmartwatch /> },
+  ];
+
+  function display() {
+    setViewAll(!viewAll);
+  }
 
   return (
     <div className="p-6">
       <div className="mb-4">
         <h2 className="text-xl font-semibold text-red-500">Categories</h2>
+      </div>{" "}
+      <div className="flex items-center justify-between">
+        <h2 className="text-2xl font-bold mb-6">Browse by Category</h2>{" "}
+        <button onClick={display} className="p-1 rounded bg-red-500">
+          View all Category
+        </button>
       </div>
-      <h1 className="text-3xl font-bold mb-6">Browse by Category</h1>
-      <div className="grid grid-cols-3 gap-6">
-        {categories.map((category, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center justify-center p-4 text-center border rounded-lg space-y-2 bg-gray-100 hover:bg-red-500 hover:text-white transition-all duration-300 cursor-pointer"
-          >
-            <div className="text-3xl">{category.icon}</div>
-            <span className="text-lg font-medium">{category.name}</span>
-          </div>
-        ))}
-      </div>
+      {viewAll ? (
+        <div className="grid grid-cols-3 gap-6">
+          {categories.map((category, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-center p-4 text-center border rounded-lg space-y-2 bg-gray-100 hover:bg-red-500 hover:text-white transition-all duration-300 cursor-pointer"
+            >
+              <div className="text-3xl">{category.icon}</div>
+              <span className="text-lg font-medium">{category.name}</span>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="grid grid-cols-3 gap-6">
+          {category.map((category, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-center p-4 text-center border rounded-lg space-y-2 bg-gray-100 hover:bg-red-500 hover:text-white transition-all duration-300 cursor-pointer"
+            >
+              <div className="text-3xl">{category.icon}</div>
+              <span className="text-lg font-medium">{category.name}</span>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
